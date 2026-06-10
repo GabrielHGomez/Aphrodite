2861db787ebd462da155c1711b8acb6f78439193
GabrielHGomez <beto.mez008@gmail.com>
2026-06-09 21:31:37 -0500

    basic texture format class

---
+++ src/texture.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <glad/glad.h>
+#include <string_view>
+
+class texture{
+public:
+  texture(std::string_view);
+  ~texture();
+  void bind();
+  int width() const {return width_;}
+  int height() const {return height_;}
+  int channels() const {return channels_;}
+  unsigned int texture_id() const {return texture_id_;}
+
+private:
+  int width_ = 0;
+  int height_ = 0;
+  int channels_ = 0;
+  unsigned int texture_id_ = 0;
+
+};
