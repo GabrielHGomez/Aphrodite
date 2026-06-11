837ee5dab500eee06ee8d6a25c7c4532ab895222
GabrielHGomez <beto.mez008@gmail.com>
2026-06-10 20:41:22 -0500

    did some moving around to be more standard :3, note to self change the cmake so I dont need the ../ in the src files

---
+++ include/window.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <string_view>
+#include <string>
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
+
+//should I make this user inputted? I dont know
+//int SRC_WIDTH = 800;
+//int SRC_HEIGHT = 600;
+
+class Window{
+public:
+  Window(std::string_view title, int width, int height);
+
+  ~Window();
+
+  int width() const {return width_;}
+  int height() const {return height_;}
+  bool close() const {return close_;}
+
+  void pollEvents();
+
+private:
+  SDL_Window* window_ = nullptr;
+  SDL_GLContext ctx_ = nullptr;
+  int width_ = 0;
+  int height_ = 0;
+  std::string_view title_ = "";
+  bool close_ = false;
+};
